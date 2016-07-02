f12a1a2f7cc7124758a3ceadd1f72ca2d50160ec
ulmerkott <eric.svard@gmail.com>
2016-07-02 14:33:08 +0200

    Added a new database from fineli.fi and a script to scrape their website for info about different portions and update the db with this new info. App is not yet updated to use this new DB.

---
+++ fineli_scrape_portions.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Script to update a the livsmedel DB with information about portion sizes from the fineli website.
+
+DATABASE_FILE=LivsmedelsDB_fineli.sqlite
+SCRAPE_URL_BASE="https://fineli.fi/fineli/sv/elintarvikkeet/"
+SCRAPE_URL_END="?foodType=ANY&portionUnit=G&portionSize=100&sortByColumn=name&sortOrder=asc&component=2331&"
+
+IFS=$'\n'
+size=`echo 'SELECT COUNT(id) FROM livsmedel;' | sqlite3 $DATABASE_FILE`
+count=1
+for id in `echo 'SELECT id FROM livsmedel;' | sqlite3 $DATABASE_FILE`; do
+    # Uncomment the following to resume from a specific id if previous scraping failed because of a hanging wget call.
+    # RESTART_FROM=1500
+    # if [ $count -lt $RESTART_FROM ]; then
+    #     let count=count+1
+    #     continue
+    # fi
+
+    atName=true
+    separator=""
+    portionString=""
+    for line in `wget -q -O - "$SCRAPE_URL_BASE$id$SCRAPE_URL_END" |grep Portionsstorlek -A 100 |grep portionSize -A 2 |grep -v "href\| 100 g\|a>\|\-\-" | sed -e 's/^[[:space:]]*//'`; do
+        if [[ $atName = true ]]; then
+            #echo NAME: $line
+            name=$line
+            atName=false
+        else
+            #echo VALUE: $line
+            value=$line
+            atName=true
+            portionString="$portionString$separator$name:$value"
+            separator=","
+        fi
+    done
+    if [ -n $portionString ]; then
+        echo "UPDATE livsmedel SET portions = \"$portionString\" WHERE id LIKE \"$id\";" | sqlite3 $DATABASE_FILE
+    else
+        echo "Portionstring was empty..."
+        exit 1
+    fi
+    echo "SCRAPING iteration $count out of $size."
+    let count=count+1
+done
+
+
